dae5a48fb05feba8d5297793296a17da6fc14539
goob30 <68452963+goob30@users.noreply.github.com>
2026-09-15 18:00:04 -0400

    add rotary functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 // ts is functionslop
 
-
 BluetoothSerial SerialBT;
 
 constexpr int BUTTON_COUNT = 4;
@@ -13,11 +12,23 @@ String buttonStatus[BUTTON_COUNT] = {"", "", "", ""};
 int ledR = 21;
 int ledG = 22;
 
-int lastLedR;
-int lastLedG;
+int lastLedR = 0;
+int lastLedG = 0;
 
 unsigned long timerLastMillis = 0;
 int timerInterval = 500;
+
+#define CW 0
+#define CCW 1
+
+int CLK_PIN = 21;
+int DT_PIN = 22;
+int ENC_SW = 23;
+
+int encCounter = 0;
+int direction = CW;
+int clk;
+int prevClk;
 
 bool isTimerTick(int interval) {
   if (millis() - timerLastMillis > interval) {
@@ -27,30 +38,13 @@ bool isTimerTick(int interval) {
   return false;
 }
 
-void setup() {
-  // set led status to uninitialized
-
-  Serial.begin(115200);
-  for (int i; i++; i < 4) {
-    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
-  }
-  SerialBT.begin();
-}
-
 String getActiveButtons() {
   String serialString;
-  for (int i; i++; i < 4) {
-    buttonStatus[i] = String(digitalRead(BUTTON_PINS[i]));
+  for (int i = 0; i < 4; i++) {
+    buttonStatus[i] = String(!digitalRead(BUTTON_PINS[i]));
     serialString += buttonStatus[i];
   }
   return serialString + "\n";
-}
-
-void blinkLeds(int interval) {
-  if (isTimerTick(interval)) {
-    lastLedR = !boolToAnalogInt(analogIntToBool(lastLedR));
-    lastLedG = !boolToAnalogInt(analogIntToBool(lastLedG));
-  }
 }
 
 int boolToAnalogInt(bool val) {
@@ -63,11 +57,20 @@ bool analogIntToBool(int val) {
   return false;
 }
 
+void blinkLeds(int interval) {
+  if (isTimerTick(interval)) {
+    lastLedR = !boolToAnalogInt(analogIntToBool(lastLedR));
+    lastLedG = !boolToAnalogInt(analogIntToBool(lastLedG));
+    analogWrite(lastLedG, ledG);
+    analogWrite(lastLedR, ledR);
+  }
+}
+
 void setColorStatus() {
   if (!SerialBT.isReady()) {
     analogWrite(ledG, 0);
     analogWrite(ledR, 255);
-    return; // if bt isnt ready its cooked
+    return;  // if bt isnt ready its cooked
   }
   if (!SerialBT.connected()) {
     blinkLeds(500);
@@ -76,6 +79,30 @@ void setColorStatus() {
   if (SerialBT.connected()) {
     analogWrite(ledR, boolToAnalogInt(true));
   }
+}
+
+String computeRotary() {
+  clk = digitalRead(CLK_PIN);
+  if (clk != prevClk && clk == 1) {
+    encCounter--;
+    direction = CCW;
+  } else {
+    encCounter++;
+    direction = CW;
+  }
+  // TODO: modify to return structured string and probably only say how many
+  // turns it went every serial print and then reset it
+}
+
+void setup() {
+  // set led status to uninitialized
+
+  Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+  }
+  SerialBT.begin("Jon Foenem");
+  prevClk = digitalRead(CLK_PIN);
 }
 
 void loop() {
